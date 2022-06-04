3f22f7a40bdb066efb6c10883c1d5747c21ca7b1
yanzt <yanzt@gistack.cn>
2022-06-04 14:54:10 +0800

    feat: 接入佳明connect跑步分析数据

--- src/constant.ts
+++ src/constant.ts
@@ -1,17 +1,27 @@
-export const COOKIE = '';
-export const CSRF_TOKEN = '';
-export const USERNAME = '';
-export const PASSWORD = '';
-export const USERID = '';
-export const HOST = 'https://www.runningquotient.cn/';
-export const UA = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36';
-export const ROUTES = {
+export const RQ_USERID_DEFAULT = '';
+export const RQ_COOKIE_DEFAULT = '';
+export const RQ_USERNAME_DEFAULT = '';
+export const RQ_PASSWORD_DEFAULT = '';
+export const RQ_CSRF_TOKEN_DEFAULT = '';
+export const RQ_HOST_DEFAULT = 'https://www.runningquotient.cn/';
+export const UA_DEFAULT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/101.0.4951.64 Safari/537.36';
+export const RQ_ROUTES_DEFAULT = {
     LOGIN: '/user/login',
     OVERVIEW: '/training/getOverView',
     UPDATE: 'training/update-overview?userId=',
 };
+export const GARMIN_URL_DEFAULT = {
+    'BASE_URL': 'https://connect.garmin.cn',
+    'SSO_URL_ORIGIN': 'https://sso.garmin.com',
+    'SSO_URL': 'https://sso.garmin.cn/sso',
+    'MODERN_URL': 'https://connect.garmin.cn/modern',
+    'SIGNIN_URL': 'https://sso.garmin.cn/sso/signin',
+    'CSS_URL': 'https://static.garmincdn.cn/cn.garmin.connect/ui/css/gauth-custom-v1.2-min.css',
+};
 
-export const G_API_CLIENT_EMAIL = '';
-export const G_API_PRIVATE_KEY = '';
-export const G_SHEET_ID = '';
-export const BARK_KEY = '';
+export const GOOGLE_API_CLIENT_EMAIL_DEFAULT = '';
+export const GOOGLE_API_PRIVATE_KEY_DEFAULT = '';
+export const GOOGLE_SHEET_ID_DEFAULT = '';
+export const BARK_KEY_DEFAULT = '';
+export const GARMIN_USERNAME_DEFAULT = '';
+export const GARMIN_PASSWORD_DEFAULT = '';
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,40 @@
 import {
-    COOKIE,
-    CSRF_TOKEN,
-    HOST,
-    ROUTES,
-    USERID,
-    G_API_CLIENT_EMAIL,
-    G_API_PRIVATE_KEY,
-    G_SHEET_ID,
-    BARK_KEY,
+    RQ_USERID_DEFAULT,
+    RQ_CSRF_TOKEN_DEFAULT,
+    RQ_COOKIE_DEFAULT,
+    RQ_HOST_DEFAULT,
+    RQ_ROUTES_DEFAULT,
+    GOOGLE_API_PRIVATE_KEY_DEFAULT,
+    GOOGLE_SHEET_ID_DEFAULT,
+    GOOGLE_API_CLIENT_EMAIL_DEFAULT,
+    BARK_KEY_DEFAULT,
+    GARMIN_USERNAME_DEFAULT,
+    GARMIN_PASSWORD_DEFAULT, GARMIN_URL_DEFAULT, UA_DEFAULT,
 } from './constant';
 
 const axios = require('axios');
+const qs = require('qs');
 const core = require('@actions/core');
+const FormData = require('form-data');
+const _ = require('lodash');
 const { google } = require('googleapis');
 const { JWT } = require('google-auth-library');
+const { GarminConnect } = require('@gooin/garmin-connect-cn');
 
 // console.log('testing: >>', process.env);
 
-const RQ_USERID = process.env.RQ_USERID ?? USERID;
-const RQ_COOKIE = process.env.RQ_COOKIE ?? COOKIE;
-const RQ_CSRF_TOKEN = process.env.RQ_CSRF_TOKEN ?? CSRF_TOKEN;
-const GOOGLE_SHEET_ID = process.env.GOOGLE_SHEET_ID ?? G_SHEET_ID;
-const GOOGLE_API_CLIENT_EMAIL = process.env.GOOGLE_API_CLIENT_EMAIL ?? G_API_CLIENT_EMAIL;
-const GOOGLE_API_PRIVATE_KEY = process.env.GOOGLE_API_PRIVATE_KEY ?? G_API_PRIVATE_KEY;
-const BARK_MESSAGE_KEY = process.env.BARK_KEY ?? BARK_KEY;
+const RQ_USERID = process.env.RQ_USERID ?? RQ_USERID_DEFAULT;
+const RQ_COOKIE = process.env.RQ_COOKIE ?? RQ_COOKIE_DEFAULT;
+const RQ_CSRF_TOKEN = process.env.RQ_CSRF_TOKEN ?? RQ_CSRF_TOKEN_DEFAULT;
+const GOOGLE_SHEET_ID = process.env.GOOGLE_SHEET_ID ?? GOOGLE_SHEET_ID_DEFAULT;
+const GOOGLE_API_CLIENT_EMAIL = process.env.GOOGLE_API_CLIENT_EMAIL ?? GOOGLE_API_CLIENT_EMAIL_DEFAULT;
+const GOOGLE_API_PRIVATE_KEY = process.env.GOOGLE_API_PRIVATE_KEY ?? GOOGLE_API_PRIVATE_KEY_DEFAULT;
+const BARK_KEY = process.env.BARK_KEY ?? BARK_KEY_DEFAULT;
+const GARMIN_USERNAME = process.env.GARMIN_USERNAME ?? GARMIN_USERNAME_DEFAULT;
+const GARMIN_PASSWORD = process.env.GARMIN_PASSWORD ?? GARMIN_PASSWORD_DEFAULT;
 
-export async function getOverView() {
-    const url = `${HOST}${ROUTES.UPDATE}${RQ_USERID}`;
+export async function getRQOverView() {
+    const url = `${RQ_HOST_DEFAULT}${RQ_ROUTES_DEFAULT.UPDATE}${RQ_USERID}`;
 
     console.log('url', url);
     try {
@@ -51,24 +59,24 @@ export async function getOverView() {
 
         // console.log('getOverView ', res);
         if (res?.data?.data) {
-            const rqdata = regexp(res?.data?.data);
-            await insertSheet(rqdata);
+            const rqdata = rqRegexp(res?.data?.data);
+            return rqdata;
         } else {
             console.error('ERROR at 0, 检查TOKEN');
             await axios.get(
-                `https://api.day.app/${BARK_MESSAGE_KEY}/RQ运行失败了/ERROR检查TOKEN`);
+                `https://api.day.app/${BARK_KEY}/RQ运行失败了/ERROR检查TOKEN`);
+            return {};
         }
     } catch (e) {
         console.error('ERROR, 检查TOKEN');
         await axios.get(
-            `https://api.day.app/${BARK_MESSAGE_KEY}/RQ运行失败了/${e.message}`);
+            `https://api.day.app/${BARK_KEY}/RQ运行失败了/${e.message}`);
         throw new Error(e);
     }
 
 }
 
-export const regexp = (htmlData) => {
-
+export const rqRegexp = (htmlData) => {
     const { conditionHtml, heartHtml, paceHtml, recordHtml, runlevelHtml } = htmlData;
     // console.log({ resJSON });
     // console.log({ recordHtml });
@@ -93,17 +101,28 @@ export const regexp = (htmlData) => {
     console.log('趋势', up[1]);
     console.log('趋势', upValue[1]);
 
-    return [
-        Date.now(),
-        time[1],
-        now[1],
-        load[1],
-        tired[1],
-        runLevel[1],
-        runLevelDesc[1],
-        up[1],
-        upValue[1],
-    ];
+    const result = {
+        updateAt: Date.now(),
+        rqTime: time[1],
+        rqLoad: load[1],
+        rqTired: tired[1],
+        rqRunLevelNow: now[1],
+        rqRunLevel: runLevel[1],
+        rqTrend1: up[1],
+        rqTrend2: upValue[1],
+    };
+    return result;
+    // return [
+    //     Date.now(),
+    //     time[1],
+    //     now[1],
+    //     load[1],
+    //     tired[1],
+    //     runLevel[1],
+    //     runLevelDesc[1],
+    //     up[1],
+    //     upValue[1],
+    // ];
 
 };
 
@@ -132,11 +151,115 @@ export const insertSheet = async (data) => {
     console.log(posts2);
 };
 
+// async function getGarminLoginCSRF() {
+//     const preRequestURL = `${GARMIN_URL_DEFAULT.SIGNIN_URL}?${qs.stringify({
+//         'cssUrl': GARMIN_URL_DEFAULT.CSS_URL,
+//     })}`;
+//     console.log('preRequestURL', preRequestURL);
+//     const res = await axios({
+//         url: preRequestURL,
+//         method: 'GET',
+//         headers: {
+//             // 'referer': GARMIN_URL_DEFAULT.BASE_URL + '/en-US/signin',
+//             'referer': GARMIN_URL_DEFAULT.SIGNIN_URL,
+//         },
+//     });
+//     // console.log('res', res);
+//     const csrf = (/name="_csrf" value="(\w*)"/.exec(res.data) as RegExpExecArray)[1];
+//     console.log('csrf found: ', csrf);
+//     // console.log('referer found: ', res.url);
+//     return csrf;
+// }
+
+export const getGarminStatistics = async () => {
+    const GCClient = new GarminConnect();
+// Uses credentials from garmin.config.json or uses supplied params
+    await GCClient.login(GARMIN_USERNAME, GARMIN_PASSWORD);
+    const userInfo = await GCClient.getUserInfo();
+    // console.log('userInfo', userInfo);
+
+    // Get a list of default length with most recent activities
+    const acts = await GCClient.getActivities(0, 10);
+    // console.log('acts', acts);
+    const recentRunningAct = _.filter(acts, { activityType: { typeKey: 'running' } })[0];
+    // console.log('recentRunningAct', recentRunningAct);
+
+    const {
+        activityId, // 活动id
+        activityName, // 活动名称
+        startTimeLocal, // 活动开始时间
+        distance, // 距离
+        duration, // 时间
+        averageSpeed, // 平均速度 m/s
+        averageHR, // 平均心率
+        maxHR, // 最大心率
+        averageRunningCadenceInStepsPerMinute, // 平均每分钟步频
+        aerobicTrainingEffect, // 有氧效果
+        anaerobicTrainingEffect, // 无氧效果
+        avgGroundContactTime, // 触地时间
+        avgStrideLength, // 步幅
+        vO2MaxValue, // VO2Max
+        avgVerticalOscillation, // 垂直振幅
+        avgVerticalRatio, // 垂直振幅比
+        avgGroundContactBalance, // 触地平衡
+        trainingEffectLabel, // 训练效果
+        activityTrainingLoad, // 训练负荷
+    } = recentRunningAct;
+
+    const pace = 1 / (averageSpeed / 1000 * 60);
+    const pace_min = Math.floor(1 / (averageSpeed / 1000 * 60));
+    const pace_second = (pace - pace_min) * 60;
+    // console.log('pace', pace);
+    // console.log('pace_min', pace_min);
+    // console.log('pace_second', pace_second);
+
+    return {
+        activityId, // 活动id
+        activityName, // 活动名称
+        startTimeLocal, // 活动开始时间
+        distance, // 距离
+        duration, // 持续时间
+        // averageSpeed 是 m/s
+        averageSpeed, // 速度
+        averagePace: pace,  // min/km
+        averagePaceText: `${pace_min}:${pace_second.toFixed(0)}`,  // min/km
+        averageHR, // 平均心率
+        maxHR, // 最大心率
+        averageRunningCadenceInStepsPerMinute, // 平均每分钟步频
+        aerobicTrainingEffect, // 有氧效果
+        anaerobicTrainingEffect, // 无氧效果
+        avgGroundContactTime, // 触地时间
+        avgStrideLength, // 步幅
+        vO2MaxValue, // 最大摄氧量
+        avgVerticalOscillation, // 垂直振幅
+        avgVerticalRatio, // 垂直振幅比
+        avgGroundContactBalance, // 触地平衡
+        trainingEffectLabel, // 训练效果
+        activityTrainingLoad, // 训练负荷
+    };
+    // const detail = await GCClient.getActivity(recentRunningAct);
+    // console.log('detail', detail);
+};
+
+export const run = async () => {
+    const rqResult = await getRQOverView();
+    const garminStatistics = await getGarminStatistics();
+    // console.log('garminStatistics', garminStatistics);
+    const data = _.assign(rqResult, garminStatistics);
+
+    // console.log('update all data', data);
+
+    const finalResult = _.values(data);
+    // console.log('finalResult', finalResult);
+
+    await insertSheet(finalResult);
+};
+
 try {
-    getOverView();
+    run();
 } catch (e) {
     axios.get(
-        `https://api.day.app/${BARK_MESSAGE_KEY}/RQ运行失败了/${e.message}`);
+        `https://api.day.app/${BARK_KEY}/RQ运行失败了/${e.message}`);
     core.setFailed(e.message);
     throw new Error(e);
 }
