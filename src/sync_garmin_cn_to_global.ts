71ba5b12fc80e7126426b4bb15f238bc25f333f4
gooin <gooin@outlook.com>
2022-09-01 21:49:51 +0800

    feat: add sync garmin global to cn

--- src/sync_garmin_cn_to_global.ts
+++ src/sync_garmin_cn_to_global.ts
@@ -1,6 +1,5 @@
 import { BARK_KEY_DEFAULT } from './constant';
 import { syncGarminCN2GarminGlobal } from './utils/garmin_cn';
-import { syncGarminGlobal2GarminCN } from './utils/garmin_global';
 
 const axios = require('axios');
 const core = require('@actions/core');
@@ -8,7 +7,6 @@ const BARK_KEY = process.env.BARK_KEY ?? BARK_KEY_DEFAULT;
 
 try {
     syncGarminCN2GarminGlobal();
-    // syncGarminGlobal2GarminCN()
 } catch (e) {
     axios.get(
         `https://api.day.app/${BARK_KEY}/Garmin CN -> Garmin Global 同步数据运行失败了，快去检查！/${e.message}`);
