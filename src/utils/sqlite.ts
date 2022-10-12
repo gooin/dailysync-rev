483ff1160cfcb83c5733d7d25fd1dd4fcb08ef84
gooin <gooin@outlook.com>
2022-10-12 10:09:55 +0800

    fix: sqlite multi user session

--- src/utils/sqlite.ts
+++ src/utils/sqlite.ts
@@ -1,13 +1,17 @@
-import { AESKEY_DEFAULT, DB_FILE_PATH, DOWNLOAD_DIR } from '../constant';
+import { AESKEY_DEFAULT, DB_FILE_PATH, DOWNLOAD_DIR, GARMIN_USERNAME_DEFAULT } from '../constant';
 import sqlite3 from 'sqlite3';
 import { Database, open } from 'sqlite';
-const CryptoJS = require("crypto-js");
+
+const CryptoJS = require('crypto-js');
+
+const GARMIN_USERNAME = process.env.GARMIN_USERNAME ?? GARMIN_USERNAME_DEFAULT;
 const AESKEY = process.env.AESKEY ?? AESKEY_DEFAULT;
 
 export const initDB = async () => {
     const db = await getDB();
     await db.exec(`CREATE TABLE IF NOT EXISTS garmin_session (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user VARCHAR(20),
             region VARCHAR(20),
             session  TEXT
         )`);
@@ -22,26 +26,30 @@ export const getDB = async () => {
 
 export const saveSessionToDB = async (type: 'CN' | 'GLOBAL', session: Record<string, any>) => {
     const db = await getDB();
-    const encryptedSessionStr = encryptSession(session)
+    const encryptedSessionStr = encryptSession(session);
     await db.run(
-        `INSERT INTO garmin_session (region,session) VALUES (?,?)`,
-        type, encryptedSessionStr
+        `INSERT INTO garmin_session (user,region,session) VALUES (?,?,?)`,
+        GARMIN_USERNAME, type, encryptedSessionStr,
     );
 };
 
 export const updateSessionToDB = async (type: 'CN' | 'GLOBAL', session: Record<string, any>) => {
     const db = await getDB();
-    const encryptedSessionStr = encryptSession(session)
+    const encryptedSessionStr = encryptSession(session);
     await db.run(
-        'UPDATE garmin_session SET session = ? WHERE region = ?',
+        'UPDATE garmin_session SET session = ? WHERE user = ? AND region = ?',
         encryptedSessionStr,
-        type
+        GARMIN_USERNAME,
+        type,
     );
 };
 
 export const getSessionFromDB = async (type: 'CN' | 'GLOBAL'): Promise<Record<string, any> | undefined> => {
     const db = await getDB();
-    const queryResult = await db.get('SELECT session FROM garmin_session WHERE region = ?', type);
+    const queryResult = await db.get(
+        'SELECT session FROM garmin_session WHERE user = ? AND region = ? ',
+        GARMIN_USERNAME, type,
+    );
     if (!queryResult) {
         return undefined;
     }
@@ -53,9 +61,9 @@ export const getSessionFromDB = async (type: 'CN' | 'GLOBAL'): Promise<Record<st
 export const encryptSession = (session: Record<string, any>): string => {
     const sessionStr = JSON.stringify(session);
     return CryptoJS.AES.encrypt(sessionStr, AESKEY).toString();
-}
+};
 export const decryptSession = (sessionStr: string): Record<string, any> => {
     const bytes = CryptoJS.AES.decrypt(sessionStr, AESKEY);
     const session = bytes.toString(CryptoJS.enc.Utf8);
     return JSON.parse(session);
-}
+};
